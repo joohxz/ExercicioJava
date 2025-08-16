841dd05b3ed661c34d5e389d40206c85bb25147c
João Moraes <joohxz@gmail.com>
2025-08-16 08:22:23 -0300

    feat: Criação e implementação da classe Funcionário

---
+++ Funcionario.java
@@ -0,0 +1,32 @@
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario extends Pessoa {
+    private BigDecimal Salario;
+    private String Funcao;
+
+    public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao){
+        super(nome, dataNascimento);
+        this.Salario = salario;
+        this.Funcao = funcao;
+    }
+
+    //Getters
+    public BigDecimal getSalario() {
+        return this.Salario;
+    }
+
+    public String getString() {
+        return this.Funcao;
+    }
+
+    //Setters
+    public void setSalario(BigDecimal salario) {
+        this.Salario = salario;
+    }
+
+    public void setFuncao(String funcao) {
+        this.Funcao = funcao;
+    }
+}
